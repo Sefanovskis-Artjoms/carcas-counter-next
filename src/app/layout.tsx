c7714743fd7f8cc93151024c730aa7c4bb77e5e2
Sefanovskis-Artjoms <artjoms200@gmail.com>
2026-06-13 11:13:08 +0300

    Fix buid issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import InternalNavigationTracker from "@/components/InternalNavigationTracker/InternalNavigationTracker";
 import ReactQueryProvider from "@/providers/ReactQueryProvider";
+import { Suspense } from "react";
 import "./globals.scss";
 import { Toaster } from "sonner";
 
@@ -12,7 +13,9 @@ export default function RootLayout({
     <html lang="en">
       <body className="text-[1.8rem]" suppressHydrationWarning={true}>
         <ReactQueryProvider>
-          <InternalNavigationTracker />
+          <Suspense fallback={null}>
+            <InternalNavigationTracker />
+          </Suspense>
           <Toaster richColors position="top-center" duration={5000} />
           <div className="main-container">{children}</div>
         </ReactQueryProvider>
